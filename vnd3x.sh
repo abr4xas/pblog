241bf8e0aecfb6ede64f7a26136f8e5c484cbd20
abr4xas <me@abr4xas.org>
2014-12-10 08:51:25 -0500

    Actualizado

--- vnd3x.sh
+++ vnd3x.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-rm output.tar.gz && tar -zcvf output.tar.gz output && cp output.tar.gz /var/www/ -v
+rm output.tar.gz && tar -zcvf output.tar.gz output && cp output.tar.gz /var/www/ -v && rm -r output
 exit 0
