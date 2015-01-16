c9d0c97cb93a11a4849fefaf8024830d8654324a
abr4xas <me@abr4xas.org>
2015-01-15 23:51:15 -0500

    Actualizando repo

--- vnd3x.sh
+++ vnd3x.sh
@@ -3,5 +3,6 @@
 # Ejecutar para actualizar los submodulos que contenga el repo
 #
 
-	git submodule foreach git pull origin master
+git submodule foreach git pull origin master && git pull origin master && git log > CHANGELOG && git add . && git commit -m "Actualizando repo" && git push origin master
+
 exit 0
