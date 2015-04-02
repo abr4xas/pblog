b1e52d2e00692943b4c3071f7b0425ad93c781c9
abr4xas <me@abr4xas.org>
2015-04-02 15:51:02 -0430

    Actualizado

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,9 +3,9 @@
 from __future__ import unicode_literals
 AUTHOR = u'abr4xas'
 SITENAME = u'El blog de abr4xas'
-SITEURL = 'http://blog.abr4xas.org'
+SITEURL = '//blog.abr4xas.org'
 # Development
-#SITEURL = ''
+#SITEURL = '//192.168.30.10/pblog/output/'
 TIMEZONE = 'America/Caracas'
 DEFAULT_LANG = u'es'
 SITE_LANG = 'es_ES'
@@ -23,9 +23,12 @@ PUBLISHER = 'https://plus.google.com/+Abr4xasOrg'
 RELATED_POSTS_MAX = 10
 GOOGLE_ANALYTICS = 'UA-56619331-1'
 GOOGLE_SV = '858AWu06X8qc7TbbcoxGVUySmw5YVEvau_yPyr3_QKI'
+ALEXA = '1zzoXK9co8WU3QqIDIWkiNC1Li0'
+MY_WOT = '5b964f1b4f47785f7e24'
+MSVALIDATE = 'F42B6441E13617A9043193F830381A74'
 SITE_DESCRIPTION = 'El blog de abr4xas, blog sobre noticias, tecnología, música, gnu/linux, anime, actualidad, intercambio y directorio de informacion de servicios profesionales en tecnología. Eventos, Asesorias Software Libre'
 TAG_LINE = '# nano /var/log/life.log'
-GOOGLE_CUSTOM_SEARCH = 'your search engine unique ID'
+DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives', '404')
 # Blogroll
 LINKS =  (
 		    ('xombra', 'http://xombra.com/'),
@@ -40,6 +43,7 @@ SOCIAL = (
 )
 PLUGIN_PATHS = ["plugins",]
 PLUGINS = ['minification','sitemap',]
+#PLUGINS = ['sitemap',]
 SITEMAP = {
 'format': 'xml',
     'priorities': {
@@ -53,19 +57,35 @@ SITEMAP = {
         'pages': 'monthly'
     }
 }
-STATIC_PATHS = ['static/browserconfig.xml',
-		'static/large.png',
-		'static/square.png',
-		'static/tiny.png',
-		'static/wide.png',
-        'static/socialmedia.txt',
-        'static/.htaccess',]
+STATIC_PATHS = [
+    'static/browserconfig.xml',
+    'static/large.png',
+    'static/square.png',
+    'static/tiny.png',
+    'static/wide.png',
+    'static/socialmedia.txt',
+    'static/.htaccess',
+]
 EXTRA_PATH_METADATA = {
-    'static/browserconfig.xml': {'path': 'browserconfig.xml'},
-    'static/large.png': {'path': 'large.png'},
-    'static/square.png': {'path': 'square.png'},
-    'static/tiny.png': {'path': 'tiny.png'}, 
-    'static/wide.png': {'path':'wide.png'},
-    'static/socialmedia.txt': {'path':'socialmedia.txt'}, 
-    'static/.htaccess': {'path':'.htaccess'},     
+    'static/browserconfig.xml': {
+        'path': 'browserconfig.xml'
+    },
+    'static/large.png': {
+        'path': 'large.png'
+    },
+    'static/square.png': {
+        'path': 'square.png'
+    },
+    'static/tiny.png': {
+        'path': 'tiny.png'
+    }, 
+    'static/wide.png': {
+        'path':'wide.png'
+    },
+    'static/socialmedia.txt': {
+        'path':'socialmedia.txt'
+    }, 
+    'static/.htaccess': {
+        'path':'.htaccess'
+    },     
 }
