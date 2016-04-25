73b90effe29bec1f1bb22f698b79678979e02a16
ángel <abr4xas@users.noreply.github.com>
2016-04-25 17:06:38 -0430

    Update vnd3x.sh

--- vnd3x.sh
+++ vnd3x.sh
@@ -3,6 +3,5 @@
 # Ejecutar para actualizar los submodulos que contenga el repo
 #
 
-git submodule foreach git pull origin master && git pull origin master && git log > CHANGELOG && git add . && git commit -m "Actualizando repo" && git push origin master
-
+git submodule foreach git pull origin master
 exit 0
